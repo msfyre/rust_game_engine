2f9a2793dda38c1b010c7f38edbec38fc7497f00
msfyre <moonstonefyre@gmail.com>
2026-03-25 17:43:40 +0800

    new start
    Signed-off-by: msfyre <moonstonefyre@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,1 @@
-mod engine;
-mod types;
-
-use types::VectorUnsigned;
-
-fn main() {
-    const WINDOW_NAME: &'static str = "ENGINE";
-    const WINDOW_SIZE: VectorUnsigned = VectorUnsigned { x: 250, y: 250 };
-    const REFRESH_RATE: u64 = 256;
-
-    let mut game_engine: engine::Engine = engine::Engine::init(WINDOW_NAME, WINDOW_SIZE);
-    game_engine.run(REFRESH_RATE);
-}
+fn main() {}
--- src/types.rs
+++
@@ -1,9 +0,0 @@
-pub struct VectorFloat {
-    pub x: f32,
-    pub y: f32,
-}
-
-pub struct VectorUnsigned {
-    pub x: u32,
-    pub y: u32,
-}
