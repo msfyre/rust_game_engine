cba0cafe1c128bdd082ad1494caec6a7c74b9dec
msfyre <moonstonefyre@gmail.com>
2026-03-25 15:55:38 +0800

    runtime methods
    Signed-off-by: msfyre <moonstonefyre@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,13 @@
 mod engine;
 mod types;
 
-use std::{self, time::Duration};
-
-use sdl3::pixels::Color;
-
 use types::VectorUnsigned;
 
 fn main() {
     const WINDOW_NAME: &'static str = "ENGINE";
     const WINDOW_SIZE: VectorUnsigned = VectorUnsigned { x: 250, y: 250 };
-    const WINDOW_BASE_COLOR: Color = Color::RGB(255, 255, 255);
+    const REFRESH_RATE: u64 = 256;
 
     let mut game_engine: engine::Engine = engine::Engine::init(WINDOW_NAME, WINDOW_SIZE);
-    game_engine.window_canvas.set_draw_color(WINDOW_BASE_COLOR);
-    game_engine.window_canvas.clear();
-    game_engine.window_canvas.present();
-
-    std::thread::sleep(Duration::from_secs(5));
+    game_engine.run(REFRESH_RATE);
 }
--- src/types.rs
+++ src/types.rs
@@ -7,17 +7,3 @@ pub struct VectorUnsigned {
     pub x: u32,
     pub y: u32,
 }
-
-// Runtime Types
-trait Callback<Args> {
-    fn call(&self, args: Args);
-}
-
-impl<Func, Args> Callback<Args> for Func
-where
-    Func: Fn(Args),
-{
-    fn call(&self, args: Args) {
-        self(args);
-    }
-}
